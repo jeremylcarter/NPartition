4ddcb15b154db076e4ab4108a52cc98a9421dd45
jeremychild <jeremychild@gmail.com>
2014-11-05 15:46:18 +1100

    Initial Code Commit

---
+++ NPartition.Service/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPartition.Service
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+
+            try
+            {
+                if (WindowsServiceHelper.ManageServiceIfRequested(Environment.GetCommandLineArgs()))
+                    return;
+            }
+            catch (Exception)
+            {
+            }
+
+            if (Environment.CommandLine.Contains("-console"))
+            {
+                WindowsServiceHelper.RunAsConsoleIfRequested<NPartitionService>();
+            }
+            else
+            {
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[] 
+                { 
+                    new NPartitionService()
+                };
+
+                ServiceBase.Run(ServicesToRun);
+            }
+
+        }
+    }
+}
